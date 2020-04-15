a2c7a1504ebc1c6ed4eec8041047778589d1a068
devisupriyadi <devisupriyadi0310@gmail.com>
2020-04-16 00:09:10 +0700

    progres

--- app/src/main/java/com/example/latihan2/regis.kt
+++ app/src/main/java/com/example/latihan2/regis.kt
@@ -2,11 +2,7 @@ package com.example.latihan2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AbsListView
-import android.widget.ArrayAdapter
-import android.widget.RadioButton
-import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_regis.*
 
 class regis : AppCompatActivity() {
 
@@ -14,33 +10,6 @@ class regis : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regis)
 
-        val JobSpinner: Spinner = findViewById(R.id.job_spinner)
-        val bloodSpinner: Spinner = findViewById(R.id.spn_blood_regis)
-        val rhesusSpinner: Spinner = findViewById(R.id.spn_rhesus_regis)
-
-        ArrayAdapter.createFromResource(
-            this, R.array.job_array,R.layout.spinner_text_style
-        ).also {adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            JobSpinner.adapter = adapter
-        }
-        ArrayAdapter.createFromResource(
-            this, R.array.arr_blood_regis,R.layout.spinner_text_style
-        ).also {adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            bloodSpinner.adapter = adapter
-        }
-        ArrayAdapter.createFromResource(
-            this, R.array.arr_rhesus_regis,R.layout.spinner_text_style
-        ).also {adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            rhesusSpinner.adapter = adapter
-        }
-    }
-
-    fun onRadioButtonClicked(view: View){
-        if(view is RadioButton){
-            val checked = view.isChecked
-        }
+        toolbars.setNavigationIcon(R.drawable.ic_header_back)
     }
 }
